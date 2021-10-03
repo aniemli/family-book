3c5b72ff32610b5af5aea8b745ce3fe5195dea25
aniemli <85290941+aniemli@users.noreply.github.com>
2021-10-03 16:21:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+var images = ["family.png","me .png","dad.png","mom.png","brother.png"];
+var names = ["My Family","Me", "Dad", "Mom", "My Brother"];
+var i = 0;
+function update()
+{
+    i++;
+    var numbers_of_family_member_in_array = 5
+    if(i > numbers_of_family_member_in_array )
+    {
+        i = 0;
+    }
+    var updatedImage = images[i];
+    var updatedName  = names[i];
+    document.getElementById("family_member_image").src = updatedImage;
+    document.getElementById("family_member_name").innerHTML = updatedName;
+}
